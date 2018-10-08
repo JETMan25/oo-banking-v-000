7eff27b84f0aac75a27e1c9217dfeb1f206da942
jontenthoff <jontenthoff@hotmail.com>
2018-10-08 19:20:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,8 +9,26 @@ class Transfer
     @status = "pending"
   end
   
+  def valid? 
+    @sender.valid? && @receiver.valid? ? true : false 
+  end
   
+  def execute_tranaction
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected.  Please check your account balance."
+    end
+  end
   
-  
-  
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
 end
